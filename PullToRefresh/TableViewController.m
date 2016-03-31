ebf8e153f1919cb48c59901a17a2f292edc4a00d
mrapplication <contact@mrapplication.com>
2016-03-31 14:34:36 +0430

    message
    first commit in github desktop application

--- PullToRefresh/TableViewController.m
+++ PullToRefresh/TableViewController.m
@@ -129,11 +129,10 @@
     double delayInSeconds = 10.0;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
     
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void)
-                   {
-                       NSLog(@"Done refresh");
-                       [self.refreshControl endRefreshing];
-                   });
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        NSLog(@"Done refresh");
+        [self.refreshControl endRefreshing];
+    });
 }
 
 #pragma mark - Table view data source
